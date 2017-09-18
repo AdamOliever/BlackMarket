e8ebe8d5dd44f81a53086b56d5974328988891ec
Tan <976660802@qq.com>
2017-09-18 17:49:33 +0800

    Create Project

---
+++ BlackMarket/BlackMarket/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  BlackMarket
+//
+//  Created by 苗盼盼 on 17/9/18.
+//  Copyright © 2017年 Adam. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
